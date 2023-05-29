3d398fccecd3512727ac9a5e403ec78c9f295831
MewPK <milzaza123@gmail.com>
2023-05-29 22:23:05 +0700

    update : topic about Jagged Array

--- Level2/Program.cs
+++ Level2/Program.cs
@@ -28,3 +28,54 @@ Console.WriteLine("Teacher Cols {0}", teacher.GetLength(1));
 Console.WriteLine("Teacher Total Length {0}", teacher.Length);
 Console.ReadKey();
 
+// foreach
+
+foreach (string name in person)
+{
+
+    Console.WriteLine("Person Name {0}", name);
+
+}
+Console.ReadKey();
+
+
+// Jagged Array
+
+int[][] list_number = new int[5][]; // fix rows 
+
+// Also You can  fix columns
+
+list_number[0] = new int[5]; // fix row 0 has 5 columns
+
+// Assign Value
+
+list_number[1] = new int[] { 10, 20, 30, 40, 50 };
+
+// or 
+double[][] list_double = new double[3][]
+{
+    new double[]{0.4,0.5},
+    new double[]{0.4,0.5},
+    new double[]{0.4,0.5}
+};
+
+// or
+
+string[][] list_str = 
+{
+    new string[]{"Hello" , "Mewpk"},
+    new string[]{"Hello" , "Mewpk"},
+    new string[]{"Hello" , "Mewpk"}
+};
+
+
+// for loop get data
+
+for (int row = 0 ; row < list_str.Length ; row++){
+
+    for(int col = 0 ; col < list_str[row].Length ; col++){
+        Console.WriteLine("String array [{0}][{1}] = {2} " , row,col ,list_str[row][col]);
+    }
+
+}
+Console.ReadKey();
