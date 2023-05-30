8ed3e38f00f42f9a7a5ce3168bbf76b0777c26df
MewPK <milzaza123@gmail.com>
2023-05-31 00:34:48 +0700

    format : file !

--- Level3/Program.cs
+++ Level3/Program.cs
@@ -7,19 +7,18 @@ namespace ConsoleApp
         public static void Main(string[] args)
         {
             // Create object Class
-            Student student1 = new Student("mewpk" , 20);
-            Console.WriteLine("Hello Object Class {0}" , student1);
-            Console.WriteLine("Name: {0} " , student1.Name);
-            Console.WriteLine("Age: {0} " , student1.Age);
-            Console.WriteLine("Id: {0} " , student1.ID);
+            Student student1 = new Student("mewpk", 20);
+            Console.WriteLine("Hello Object Class {0}", student1);
+            Console.WriteLine("Name: {0} ", student1.Name);
+            Console.WriteLine("Age: {0} ", student1.Age);
+            Console.WriteLine("Id: {0} ", student1.ID);
             Console.ReadKey();
-            Student student2 = new Student("pk" , 10);
-            Console.WriteLine("Hello Object Class {0}" , student2);
-            Console.WriteLine("Name: {0} " , student2.Name);
-            Console.WriteLine("Age: {0} " , student2.Age);
-            Console.WriteLine("Id: {0} " , student2.ID);
+            Student student2 = new Student("pk", 10);
+            Console.WriteLine("Hello Object Class {0}", student2);
+            Console.WriteLine("Name: {0} ", student2.Name);
+            Console.WriteLine("Age: {0} ", student2.Age);
+            Console.WriteLine("Id: {0} ", student2.ID);
             Console.ReadKey();
         }
     }
 }
- 
--- Level3/Student.cs
+++ Level3/Student.cs
@@ -3,21 +3,21 @@ using System;
 namespace ConsoleApp
 {
 
-   class Student
-{
-    private static int nextId = 0;
-    
-    public Student(string name, int age)
+    class Student
     {
-        Name = name;
-        Age = age;
-        ID = ++nextId;
-    }
+        private static int nextId = 0;
 
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public readonly int ID;
-}
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            ID = ++nextId;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public readonly int ID;
+    }
 
 
 }
