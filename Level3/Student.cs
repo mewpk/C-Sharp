e7aae4c62207c74a9e9033cdf63a36ad74e8432c
MewPK <milzaza123@gmail.com>
2023-05-30 22:40:12 +0700

    update : Auto-implemented properties are used for Name and Age properties.

--- Level3/Student.cs
+++ Level3/Student.cs
@@ -2,8 +2,21 @@ using System;
 
 namespace ConsoleApp
 {
-    public class Student
-    {
+   
+    class Student
+{
+    private static int nextId = 0;
     
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+        ID = ++nextId;
     }
+    
+    public string Name { get; }
+    public int Age { get; }
+    public int ID { get; } 
+}
+
 }
